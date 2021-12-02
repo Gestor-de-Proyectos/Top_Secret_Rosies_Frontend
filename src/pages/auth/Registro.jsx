0b2f3b63fca51ec1af4fa3a285f067a2fc3dbfda
Daniela1212 <danielabmed12@gmail.com>
2021-12-02 15:43:11 -0500

    Hooks UseFormData

--- src/pages/auth/Registro.jsx
+++ src/pages/auth/Registro.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Registro = () => {
-    return (
-        <>
-    <div className='max-w-md w-full space-y-8'>
+import Input from 'components/input';
+import useFormData from 'hooks/useFormData';
+
+function Registro() {
+  const { form, formData, updateFormData } = useFormData();
+
+  const submitForm = (e)=>{
+    e.preventDefault();
+  };
+  
+  return (
+    <>
+      <div className='max-w-md w-full space-y-8'>
         <h2 className='mt-6 text-center text-3xl font-bold text-gray-900'>
           Crea tu cuenta
         </h2>
-      <form className='mt-8 space-y-6'>        
+        <form className='mt-8 space-y-6'>
           <label htmlFor='nombre'>
             Nombre
             <input
@@ -17,8 +26,7 @@ const Registro = () => {
               autoComplete='email'
               required
               className='appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-green-500 focus:border-green-500 focus:z-10 sm:text-sm'
-              placeholder='Yadira'
-            />
+              placeholder='Yadira' />
           </label>
           <label htmlFor='apellido'>
             Apellido
@@ -27,8 +35,7 @@ const Registro = () => {
               type='text'
               required
               className='appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-green-500 focus:border-green-500 focus:z-10 sm:text-sm'
-              placeholder='Henao'
-            />
+              placeholder='Henao' />
           </label>
           <label htmlFor='identificacion'>
             Número de identificación
@@ -37,8 +44,7 @@ const Registro = () => {
               type='id'
               required
               className='appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-green-500 focus:border-green-500 focus:z-10 sm:text-sm'
-              placeholder='1023452217'
-            />
+              placeholder='1023452217' />
           </label>
           <label htmlFor='correo'>
             Correo electrónico
@@ -47,8 +53,7 @@ const Registro = () => {
               type='email'
               required
               className='appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-green-500 focus:border-green-500 focus:z-10 sm:text-sm'
-              placeholder='yadira.henao@udea.edu.co'
-            />
+              placeholder='yadira.henao@udea.edu.co' />
           </label>
           <label htmlFor='contraseña'>
             Contraseña
@@ -57,45 +62,44 @@ const Registro = () => {
               type='password'
               required
               className='appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-green-500 focus:border-green-500 focus:z-10 sm:text-sm'
-              placeholder='xyu7651_u86'
-            />
+              placeholder='xyu7651_u86' />
           </label>
 
           <label htmlFor='tipo-usuario'>
             Tipo de usuario
             <select
-            name='tipo-usuario'
-            className='appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-green-500 focus:border-green-500 focus:z-10 sm:text-sm bg-gray-200'>
-            
-            <option value="1">Estudiante</option>
-            <option value="2">Líder</option>
-            <option value="3">Administrador</option>
-                
+              name='tipo-usuario'
+              className='appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-green-500 focus:border-green-500 focus:z-10 sm:text-sm bg-gray-200'>
+
+              <option value="1">Estudiante</option>
+              <option value="2">Líder</option>
+              <option value="3">Administrador</option>
+
             </select>
           </label>
-          
-                  
 
-        <div>
-          <button
-            type='submit'
-            className='group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500'
-          >
-            
-            <Link to='/admin'>Regístrate</Link>
-          </button>
-        </div>
 
-        <div className='flex items-center justify-between ml-2 block text-sm text-gray-900'>
-          <span>¿Ya tienes cuenta?</span>
-          <Link to='/'>
-            <span className='font-medium text-green-600 hover:text-green-500'>Inicia Sesión</span>
-          </Link>
-        </div>
-      </form>
+
+          <div>
+            <button
+              type='submit'
+              className='group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500'
+            >
+
+              <Link to='/admin'>Regístrate</Link>
+            </button>
+          </div>
+
+          <div className='flex items-center justify-between ml-2 block text-sm text-gray-900'>
+            <span>¿Ya tienes cuenta?</span>
+            <Link to='/'>
+              <span className='font-medium text-green-600 hover:text-green-500'>Inicia Sesión</span>
+            </Link>
+          </div>
+        </form>
       </div>
-    </>       
-    )
+    </>
+  );
 }
 
 export default Registro;

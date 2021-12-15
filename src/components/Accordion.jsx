f3e9df1020c491f462bdb2766a4e993106ede7c9
Bethsyf <bethsyfalcon@gmail.com>
2021-12-15 15:42:33 -0500

    correcciones de CCS en proyectos e inscripciones

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -4,13 +4,13 @@ import AccordionDetails from '@mui/material/AccordionDetails';
 import { styled } from '@mui/material/styles';
 
 const AccordionStyled = styled((props) => <Accordion {...props} />)(({ theme }) => ({
-  backgroundColor: '#919191',
+  backgroundColor: '#99d599', 
 }));
 const AccordionSummaryStyled = styled((props) => <AccordionSummary {...props} />)(({ theme }) => ({
-  backgroundColor: '#919191',
+  backgroundColor: '#183924',
 }));
 const AccordionDetailsStyled = styled((props) => <AccordionDetails {...props} />)(({ theme }) => ({
-  backgroundColor: '#ccc',
+  backgroundColor: '#c5cbb5',
 }));
 
 export { AccordionStyled, AccordionSummaryStyled, AccordionDetailsStyled };

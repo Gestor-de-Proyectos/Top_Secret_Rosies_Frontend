cc7c00db239e4e0c53e33def6e5af27ce9ae4db6
Daniela1212 <danielabmed12@gmail.com>
2021-12-17 15:54:28 -0500

    Prueba unitaria PrivateRoute

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -4,11 +4,19 @@ import React from 'react';
 const PrivateRoute = ({ roleList, children }) => {
   const { userData } = useUser();
 
+
   if (roleList.includes(userData.rol)) {
     return children;
   }
 
-  return <div className='text-9xl text-red-500 '>No estás autorizado para ver este sitio.</div>;
+
+  return (
+    <div className='text-9xl text-red-500' data-testid='unauthorized'>
+      No estás autorizado para ver este sitio.
+    </div>
+    
+  );
 };
 
 export default PrivateRoute;
+

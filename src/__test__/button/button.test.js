8e4fe104bb32f9456cf73e361f46d4286937bae9
Daniela1212 <danielabmed12@gmail.com>
2021-12-17 16:15:07 -0500

    Corrección prueba Button

--- src/__test__/button/button.test.js
+++ src/__test__/button/button.test.js
@@ -6,36 +6,36 @@ import { render, cleanup, screen } from '@testing-library/react';
 
 afterEach(cleanup);
 it('renders okay', () => {
-  render(<ButtonLoading loading={false} text={'Hola'} />);
+  render(<ButtonLoading loading={false} text={'Prueba'} />);
   expect(screen.getByTestId('button-test')).toBeInTheDocument();
 });
 
 
 it('renders button correctly without loading', () => {
-  render(<ButtonLoading loading={false} text={'Hola'} />);
-  expect(screen.getByTestId('button-test')).toHaveTextContent('Hola');
+  render(<ButtonLoading loading={false} text={'Prueba'} />);
+  expect(screen.getByTestId('button-test')).toHaveTextContent('Prueba');
 });
 
 
 it('renders button correctly with loading', () => {
-  render(<ButtonLoading loading={true} text={'Hola'} />);
-  expect(screen.getByTestId('button-test')).not.toHaveTextContent('Hola');
+  render(<ButtonLoading loading={true} text={'Prueba'} />);
+  expect(screen.getByTestId('button-test')).not.toHaveTextContent('Prueba');
 });
 
 
 it('renders button correctly disabled', () => {
-  render(<ButtonLoading disabled={true} loading={false} text={'Hola'} />);
+  render(<ButtonLoading disabled={true} loading={false} text={'Prueba'} />);
   expect(screen.getByTestId('button-test')).toHaveAttribute('disabled');
 });
 
 it('renders button correctly enabled', () => {
-  render(<ButtonLoading disabled={false} loading={false} text={'Hola'} />);
+  render(<ButtonLoading disabled={false} loading={false} text={'Prueba'} />);
   expect(screen.getByTestId('button-test')).not.toHaveAttribute('disabled');
 });
 
 
 it('renders loading', () => {
-  render(<ButtonLoading loading={true} disabled={false} text={'Hola'} />);
+  render(<ButtonLoading loading={true} disabled={false} text={'Prueba'} />);
   expect(screen.getByTestId('button-loading-test')).toBeInTheDocument();
 });
 
